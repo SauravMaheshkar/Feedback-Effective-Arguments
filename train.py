c9a8ab996c1c56c722f831d9d309cbcec106c9f0
Saurav Maheshkar <saurav@wandb.com>
2022-06-17 20:57:00 +0000

    feat: Add Artifacts and improved optimizer code

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 """Training Code"""
 import gc
+import glob
+import os
 from typing import Dict
 
 import torch
 from rich import print
-from torch.optim import AdamW
 from transformers import AutoTokenizer
 
 import wandb
@@ -12,19 +13,25 @@ from src.io.data import get_dataframe
 from src.io.dataset import prepare_loaders
 from src.nn.engine import run_training
 from src.nn.model import FeedBackModel
+from src.nn.optimizers import fetch_optimizer
 from src.nn.schedulers import fetch_scheduler
 from src.utils import set_seed
 
+if not os.path.exists("output"):
+    os.mkdir("output")
+
 # Configuration
 config: Dict = {
-    "seed": 42,
+    "seed": 7,
     "epochs": 5,
-    "model_name": "roberta-base",
+    "model_name": "roberta-large",
+    "classifier_dropout": 0.1,
     "strategy": "GroupKFold",
     "train_batch_size": 8,
     "valid_batch_size": 16,
-    "max_length": 512,
+    "max_length": 256,
     "learning_rate": 1e-5,
+    "optimizer": "AdamW",
     "scheduler": "CosineAnnealingWarmRestarts",
     "min_lr": 1e-6,
     "T_max": 500,
@@ -77,12 +84,13 @@ if __name__ == "__main__":
         print(f"Model Parameters:{trainable_model_params}")
         wandb.run.summary["Model Parameters"] = trainable_model_params  # type: ignore
 
-        # Define Optimizer and Scheduler
-        optimizer = AdamW(
-            model.parameters(),
-            lr=config["learning_rate"],
+        optimizer = fetch_optimizer(
+            model=model,
+            optimizer=config["optimizer"],
+            learning_rate=config["learning_rate"],
             weight_decay=config["weight_decay"],
         )
+
         scheduler = fetch_scheduler(
             scheduler=config["scheduler"],
             optimizer=optimizer,
@@ -116,5 +124,29 @@ if __name__ == "__main__":
     VALID_LOSS /= config["n_fold"]
     wandb.run.summary["Validation Loss"] = VALID_LOSS  # type: ignore
     print(f"Average Validation Loss: {VALID_LOSS}")
+
+    model_artifact = wandb.Artifact(
+        name=f"""{config["seed"]}seed-
+            {config["epochs"]}epochs-
+            {config["n_fold"]}folds-
+            {config["learning_rate"]}lr""",
+        type=config["model_name"],
+        metadata=config,
+    )
+    model_artifact.add_dir("output/")
+    wandb.run.log_artifact(model_artifact)
+
+    code_artifact = wandb.Artifact(
+        name=f"""{config["seed"]}seed-
+            {config["epochs"]}epochs-
+            {config["n_fold"]}folds-
+            {config["learning_rate"]}lr""",
+        type="codebase",
+        metadata=config,
+    )
+    for f in glob.glob("src/**/*.py", recursive=True):
+        code_artifact.add_file(f)
+    code_artifact.add_file("./train.py")
+    wandb.run.log_artifact(code_artifact)
 
     wandb.finish()
